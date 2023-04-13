5ce87f0bc37d5e80a7a92f481c26697abf51ebe4
NfoTECH <fortunenwachukwu@gmail.com>
2023-04-13 15:55:21 +0100

    Update: clean up the CustomerService class

--- src/main/java/com/javatechie/springbootwebfluxdemo/service/CustomerService.java
+++ src/main/java/com/javatechie/springbootwebfluxdemo/service/CustomerService.java
@@ -4,11 +4,7 @@ import com.javatechie.springbootwebfluxdemo.dao.CustomerDao;
 import com.javatechie.springbootwebfluxdemo.dto.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
